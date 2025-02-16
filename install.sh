2c0beacaf4b4caaec15d887ee41edfc9a6505902
Tjeerd Makel <tjeerd.makel@gmail.com>
2025-02-16 14:59:09 +0100

    Fix install of Python library dependencies and add systemd service

--- install.sh
+++ install.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-sudo apt install python3-pip
-pip3 install dsmr-parser
-pip3 install influxdb
+## Create virtual Python environment, as this is needed since Debian Bookworm
+python3 -m venv .
+bin/pip install influxdb
+bin/pip install dsmr-parser
 
+## Install systemd service
+sudo cp p1_to_influxdb.service /etc/systemd/system/.
+sudo systemctl enable p1_to_influxdb
+sudo systemctl daemon-reload
+sudo systemctl start p1_to_influxdb
+systemctl status p1_to_influxdb
